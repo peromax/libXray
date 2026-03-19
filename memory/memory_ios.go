95533e7c012a27d749d5832e68caa4b453ede690
Eugene Fedorov <piarmx@gmail.com>
2026-03-19 19:34:27 +0300

    try remove SetMemoryLimit for GC

--- memory/memory_ios.go
+++ memory/memory_ios.go
@@ -10,11 +10,11 @@ import (
 	// "time"
 )
 
-const (
-	// interval = 5
-	// 33M
-	maxMemory = 30 * 1024 * 1024
-)
+// const (
+// 	// interval = 5
+// 	// 33M
+// 	maxMemory = 30 * 1024 * 1024
+// )
 
 // func forceFree(interval time.Duration) {
 // 	go func() {
@@ -28,8 +28,8 @@ const (
 func InitForceFree() {
 	os.Setenv("GODEBUG", "tlsmlkem=0")
 
-	debug.SetGCPercent(10)
-	debug.SetMemoryLimit(maxMemory)
+	debug.SetGCPercent(30)
+	// debug.SetMemoryLimit(maxMemory)
 	// set golang threads num
 	runtime.GOMAXPROCS(3)
 	// duration := time.Duration(interval) * time.Second
